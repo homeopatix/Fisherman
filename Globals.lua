d577b6abc724f8105bbabe856807e2666d70b358
Daniel D <spawn900@users.noreply.github.com>
2024-02-10 20:00:14 +0100

    Update Globals.lua - Fisherman
    Updated the german translation and added "fish" to the help-command lines.
    Added "fish" to the other languages, too.
    The option "/fish clear" was missing in the help. Added it only in german.

--- Globals.lua
+++ Globals.lua
@@ -10,25 +10,26 @@ function CreateLocalizationInfo()
 	if Turbine.Engine.GetLanguage() == Turbine.Language.German then
 		Strings.PluginName = "Fish";
 		Strings.PluginText = "Von Homeopatix";
-		Strings.PluginLoisir = "Wirf die Leine";
+		Strings.PluginLoisir = "Wirf die Angel";
 		Strings.PluginPrimary = "Prim\195\164r";
 		Strings.PluginSecondary = "Sekund\195\164r";
 		Strings.PluginCanneapeche = "Angelrute";
-		Strings.PluginValidate = "Auswahl validieren";
+		Strings.PluginValidate = "Speichern";
 		Strings.PluginAltEnable = "Alt-Taste aktiviert";
 		Strings.PluginAltDesable = "Alt-Taste deaktiviert";
-		Strings.PluginOptionShowWindow = "Zeigen Sie das Optionsfenster an";
-		Strings.PluginClear = "L\195\182schen Sie die Verkn\195\188pfungen";
-		Strings.PluginWindowShow = "Schaufenster.";
-		Strings.PluginWindowHide = "Verstecke das Fenster.";
+		Strings.PluginOptionShowWindow = "Optionen werden angezeigt";
+		Strings.PluginClear = "Alle Verkn\195\188pfungen gel\195\182scht!";
+		Strings.PluginWindowShow = "Fenster wird angezeigt";
+		Strings.PluginWindowHide = "Fenster wird ausgeblendet";
 		Strings.PluginHelp = " *** Fisherman Hilfe ***\n\n" ..
-		"/show Zeigen Fisherman an\n" ..
-		"/hide Verstecke Fisherman\n" ..
-		"/alt - Aktivieren oder deaktivieren Sie die alt-Taste um das Symbol zu verschieben.\n" ..
-		"/esc - Aktivieren oder deaktivieren Sie die Escape-Taste\n" ..
-		"/options - Zeigen Sie das Optionsfenster an\n" ..
-		"/toggle - Fenster ein-oder ausblenden\n\n" ..
-		"Sie k\195\182nnen Ihre Angelrute im Symbol rechts ziehen und ablegen";
+		"/fish show - Zeigt das Fenster an.\n" ..
+		"/fish hide - Blendet das Fenster aus.\n" ..
+		"/fish alt - Alt-Taste gedr\195\188ckt halten, um das Symbol zu verschieben.\n" ..
+		"/fish clear - L\195\182scht alle Verkn\195\188pfungen.\n" ..
+		"/fish esc - Aktiviert/Deaktiviert die Escape-Taste, um das Fenster zu schlie\195\159en.\n" ..
+		"/fish options - Zeigt die Optionen an.\n" ..
+		"/fish toggle - Fenster anzeigen/ausblenden.\n\n" ..
+		"Du kannst deine Angelrute auf den rechten Platz ziehen und ablegen.";
 
 elseif Turbine.Engine.GetLanguage() == Turbine.Language.French then
 		Strings.PluginName = "Fish";
@@ -45,12 +46,12 @@ elseif Turbine.Engine.GetLanguage() == Turbine.Language.French then
 		Strings.PluginWindowShow = "Affiche la fen\195\168tre.";
 		Strings.PluginWindowHide = "Cache la fen\195\168tre.";
 		Strings.PluginHelp = " *** Fisherman Aide ***\n\n" ..
-		"/show affiche Fisherman\n" ..
-		"/hide cache Fisherman\n" ..
-		"/alt - Active ou d\195\169sactive la touche alt pour le d\195\169placement de l'icon.\n" ..
-		"/esc - active ou desactive la touche escape\n" ..
-		"/options - Affiche la fen\195\170tre d'options\n" ..
-		"/toggle - Affiche ou cache la fenetre.\n\n" ..
+		"/fish show affiche Fisherman\n" ..
+		"/fish hide cache Fisherman\n" ..
+		"/fish alt - Active ou d\195\169sactive la touche alt pour le d\195\169placement de l'icon.\n" ..
+		"/fish esc - active ou desactive la touche escape\n" ..
+		"/fish options - Affiche la fen\195\170tre d'options\n" ..
+		"/fish toggle - Affiche ou cache la fenetre.\n\n" ..
 		"Vous pouvez faire glisser et d\195\169poser votre canne a peche dans l'icon de droite";
 
 elseif Turbine.Engine.GetLanguage() == Turbine.Language.English then
@@ -68,12 +69,12 @@ elseif Turbine.Engine.GetLanguage() == Turbine.Language.English then
 		Strings.PluginWindowShow = "Show the window.";
 		Strings.PluginWindowHide = "Hide the window.";
 		Strings.PluginHelp = " *** Fisherman Help ***\n\n" ..
-		"/show Display Fisherman\n" ..
-		"/hide Hide Fisherman\n" ..
-		"/alt - Activate or deactivate the alt key to move the icon.\n" ..
-		"/esc - activate or deactivate the escape key\n" ..
-		"/options - Display the option window\n" ..
-		"/toggle - Display or hide the window.\n\n" ..
+		"/fish show Display Fisherman\n" ..
+		"/fish hide Hide Fisherman\n" ..
+		"/fish alt - Activate or deactivate the alt key to move the icon.\n" ..
+		"/fish esc - activate or deactivate the escape key\n" ..
+		"/fish options - Display the option window\n" ..
+		"/fish toggle - Display or hide the window.\n\n" ..
 		"You can drag and drop your fishing rod in the icon on the right";
 	end
 end
